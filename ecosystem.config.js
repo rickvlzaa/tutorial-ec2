1d3c043e4f74cc603dd424b7eab3fe20dbc62d87
Rick Veloz <veloz.ricardo.a@gmail.com>
2018-05-03 09:49:33 -0500

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'tutorial-ec2',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-188-255-220.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/tutorial.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:rickvlzaa/tutorial-ec2.git',
+      path: '/home/ubuntu/tutorial-ec2',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
